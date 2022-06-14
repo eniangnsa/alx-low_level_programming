4077a6aeefc701430d6128f4ed98ee5d25c3bdf7
eniangnsa <adolph.eniang@gmail.com>
2022-06-14 13:12:11 -0700

    used puts to print

--- 0x00-hello_world/4-put.c
+++ 0x00-hello_world/4-put.c
@@ -1,6 +1,5 @@
-#!/bin/bash
 #include <stdio.h>
 {
-	puts("\"Programming is like building is like a multilingual puzzle");
-	return(0);
-}	
+puts("\"Programming is like building is like a multilingual puzzle");
+return (0);
+}
