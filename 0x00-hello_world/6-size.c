bb285144ccc321078edc2eeffc37ff04fa9dfd26
eniangnsa <adolph.eniang@gmail.com>
2022-06-14 21:56:27 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-printf("the size of a char is %d\n", sizeof(char));
-printf("the size of an int is %d\n", sizeof(int));
-printf("the size of a long int is %d\n", sizeof(long int));
-printf("the size of a long long int is %d\n", sizeof(long long int));
-printf("the size of a float is %d\n", sizeof(float));
+printf("the size of a char : %d\n", sizeof(char));
+printf("the size of an int : %d\n", sizeof(int));
+printf("the size of a long int : %d\n", sizeof(long int));
+printf("the size of a long long int : %d\n", sizeof(long long int));
+printf("the size of a float : %d\n", sizeof(float));
 return (0);
 }	
