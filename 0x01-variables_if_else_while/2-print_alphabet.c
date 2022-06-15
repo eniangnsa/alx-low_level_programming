b5136ed5cec5481bc60a51b1776e597056bdfe5a
eniangnsa <adolph.eniang@gmail.com>
2022-06-15 14:26:56 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+	for (ch=97;ch<=122;ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	
+	return (0);
+}
