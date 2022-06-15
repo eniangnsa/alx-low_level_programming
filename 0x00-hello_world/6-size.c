1ec355a74ba834ba80b58773a318476ff08bbe2e
eniangnsa <adolph.eniang@gmail.com>
2022-06-14 22:06:30 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,10 +4,10 @@
  */ 
 int main(void) /* main entry */
 {
-printf("the size of a char : %d\n", sizeof(char));
-printf("the size of an int : %d\n", sizeof(int));
-printf("the size of a long int : %d\n", sizeof(long int));
-printf("the size of a long long int : %d\n", sizeof(long long int));
-printf("the size of a float : %d\n", sizeof(float));
+printf("Size of a char : %d\n", sizeof(char));
+printf("Size of a int : %d\n", sizeof(int));
+printf("Size of a long int : %d\n", sizeof(long int));
+printf("Size of a long long int : %d\n", sizeof(long long int));
+printf("Size of a float : %d\n", sizeof(float));
 return (0);
 }
