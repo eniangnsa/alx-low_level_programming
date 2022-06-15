7e1502f8d34294095c6229052585d153eca3f1da
eniangnsa <adolph.eniang@gmail.com>
2022-06-14 21:40:42 -0700

    print the sizes of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+printf("the size of a char is %d\n, sizeof(char)");
+printf("the size of an int is %d\n, sizeof(int)");
+printf("the size of a long int is %d\n, sizeof(long int)");
+prrintf("the size of a long long int is %d\n, sizeof(long long int)");
+printf("the size of a float is %d\n, sizeof(float)");
+
+}	
