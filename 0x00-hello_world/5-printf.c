c8210e5579c4cdec3ffa46e4661df20c453b77ca
eniangnsa <adolph.eniang@gmail.com>
2022-06-15 07:53:51 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-/**
- * this program prints something about grammar
- */
-
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
