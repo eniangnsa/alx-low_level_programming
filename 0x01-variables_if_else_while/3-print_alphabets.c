fe5680de68d94d01dc316aceb62a3e0a5f115832
eniangnsa <adolph.eniang@gmail.com>
2022-06-15 14:55:14 -0700

    lower and upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@
  */
 int main(void)
 {
-	int ch;
-	for (ch = 97; ch <= 122; ch++);
+int ch;
+for (ch = 97; ch <= 122; ch++);
 	{
 		putchar(ch);
 	}
-	for (ch = 65; ch <= 90; ch++);
+for (ch = 65; ch <= 90; ch++);
 	{
 		putchar(ch);
 	}
-	putchar(10);
+putchar(10);
 
-	return (0);
+return (0);
 }	
