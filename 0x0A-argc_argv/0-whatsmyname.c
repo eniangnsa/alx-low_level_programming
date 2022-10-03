184266ce65dc6d169802bc091dca5907086e6494
eniangnsa <adolph.eniang@gmail.com>
2022-10-03 01:23:35 -0700

    created a program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * this is a program that prints the name of the file
+ * @argc : argc parameter
+ * @argv : an array that stores all the parameters of the function.
+ * Return : 0 for success
+ */
+ 
+ int main(int argc, char *argv[])
+ {
+ 	printf("%s\n", argv[0]);
+	return (0);
+ }
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+The project is about adding parameters to our main function. the initial parameters we're going to be concerned with are the argc and argv.
+
+The argc tells us how many arguments are there in our functions while the argv is an array that stores all the arguments passed into the main funnction.
