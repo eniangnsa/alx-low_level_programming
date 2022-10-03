90236d20b94677bd29ca15712163c7cb7a8651ad
eniangnsa <adolph.eniang@gmail.com>
2022-10-03 11:28:56 -0700

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - a program that multiplies two numbbers
  * @argc: number of arguments passed to the main function
@@ -7,14 +8,18 @@
  */
 int main(int argc, char *argv[])
 {
+	(void) argc;
+
 	int prod, first, second;
 
-	if (argc >= 2)
+	if (argv[1] && argv[2])
 		first = atoi(argv[1]); /* to convert strinng to int */
 		second = atoi(argv[2]); /* to convert string to int */
 		prod = first * second;
-		printf('%d\n', prod);
+		printf("%d\n", prod);
+		return (0);
+
 	else
-		printf("%s\n", "Error");
-	return (0);
+		printf("Error");
+	return (1);
 }
