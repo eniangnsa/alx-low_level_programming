1c95261647ecf10632e751b1e22ae414965dce2f
eniangnsa <adolph.eniang@gmail.com>
2022-10-03 09:05:56 -0700

    removed whitespaces

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 /**
  * this is a program that prints the name of the file
  * @argc : argc parameter
  * @argv : an array that stores all the parameters of the function.
  * Return : 0 for success
  */
- 
- int main(int argc __attribute__((unused)), char *argv[])
- {
- 	printf("%c\n", *argv[0]);
+
+#include <stdio.h>
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%c\n", *argv[0]);
 	return (0);
- }
+}
