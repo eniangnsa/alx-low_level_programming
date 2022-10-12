887651773374104357b056e9251a41c9255d6999
eniangnsa <adolph.eniang@gmail.com>
2022-10-12 07:58:41 -0700

    add arguments given to it

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,18 +6,28 @@
  * @argv: an array that stores the arguments.
  * Return: 0 for success.
  */
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
-	(void) argc;
+	int total, i;
+	char *p;
+	int num;
 
-	if (argv[1] && argv[2])
-
-		printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
-		return (0);
-
-	else
-		printf("Error\n");
-
-	return (1);
+	total = 0;
+	if (argc > 1)
+	{
+		for (i = 1; argv[i]; i++)
+		{
+			num = strtol(argv[i], &p, 10);
+			if (!*p)
+				total += num;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", total);
+	return (0);
 }
 
