424a2d49aa449585c58aad57cf1da9bb2217537f
eniangnsa <adolph.eniang@gmail.com>
2022-10-13 05:45:05 -0700

    all added

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - free memory
+ * @d: pointer to a dog struct data
+ *
+ * Return: None
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name), free(d->owner);
+		free(d);
+	}
+}
