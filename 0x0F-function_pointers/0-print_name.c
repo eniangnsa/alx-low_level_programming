b73144861c9f06b10a6debcb55d34366f7d1d120
eniangnsa <adolph.eniang@gmail.com>
2022-10-14 01:32:49 -0700

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name string
+ * @name: pointer to a string of characters
+ * @f: pointer to a function
+ *
+ * Return: None
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that searches for an integer.
+ * @array: pointer to an array of integers
+ * @size: array length
+ * @cmp: pointer to a function
+ *
+ * Return: index of the first element for which cmp function does not return 0,
+ * if no element matches, return -1, if size <= 0, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array != NULL && size > 0 && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
+
